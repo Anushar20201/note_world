4308cfedb745d2e12ee0f02b04d119beebe44da6
Anusha <anushar20201@gmail.com>
2022-07-09 17:56:13 -0500

    adding delete end point

--- routes/apiRoutes/noteWorldRoutes.js
+++ routes/apiRoutes/noteWorldRoutes.js
@@ -1,25 +1,20 @@
 const router = require('express').Router();
-const { createNewNote } = require('../../lib/notesCRUD');
+const { createNewNote, deleteOldNote } = require('../../lib/notesCRUD');
 const { notes } = require('../../db/db');
 
-//getting the saved notes
+//getting the saved notes -GET method
 router.get('/notes', (req, res) => {
   let results = notes;
-  console.log("results")
-
   console.log(results)
-
-  res.json(results);
-
-//   if (results) {
-//     res.json(results);
-// } else {
-//     res.send(404);
-//   }
+  if (results) {
+    res.json(results);
+} else {
+    res.send(404);
+  }
 });
 
 
-
+//saving any new notes - POST method
 router.post('/notes', (req, res) => {
   // set id based on what the next index of the array will be
   console.log(notes.length)
@@ -27,5 +22,13 @@ router.post('/notes', (req, res) => {
     const note = createNewNote(req.body, notes);
     res.json(note);
 });
+
+
+router.delete('/notes/:id', (req, res) => {
+  deleteOldNote(notes, req.params.id);
+  res.json(notes);
+})
+
+
 
 module.exports = router;
