670e4c3cbbde6683a3af4f1b0f3f1dddab4abdb6
Anusha <anushar20201@gmail.com>
2022-07-09 16:26:29 -0500

    adding create()

--- routes/apiRoutes/noteWorldRoutes.js
+++ routes/apiRoutes/noteWorldRoutes.js
@@ -13,4 +13,17 @@ router.get('/db', (req, res) => {
 });
 
 
+
+router.post('/db', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = db.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, db);
+    res.json(animal);
+  }
+});
+
 module.exports = router;
